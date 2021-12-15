3ff0cbcd66c58fd7af1157cb5fe18a8bc40453ee
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-12-15 16:20:14 +0400

    custom func not working right

--- UrlShortener/shortener.py
+++ UrlShortener/shortener.py
@@ -3,9 +3,8 @@ from datetime import datetime
 from random import choices
 
 from flask import Flask, render_template, url_for, flash, redirect, request
-from flask_login import login_required
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from auth import requires_auth
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -16,7 +15,7 @@ db = SQLAlchemy(app)
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
-    short_url = db.Column(db.String(3), unique=True)
+    short_url = db.Column(db.String(5), unique=True)
     visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
@@ -25,8 +24,8 @@ class Link(db.Model):
         self.short_url = self.generate_short_link()
 
     def generate_short_link(self):
-        characters = string.digits + string.ascii_letters
-        short_url = ''.join(choices(characters, k=3))
+        characters = string.digits + string.ascii_uppercase + string.ascii_lowercase
+        short_url = ''.join(choices(characters, k=5))
 
         link = self.query.filter_by(short_url=short_url).first()
 
@@ -36,8 +35,30 @@ class Link(db.Model):
         return short_url
 
 
+class Link2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    spec_url = db.Column(db.String(512))
+    uniq_url = db.Column(db.String(), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.uniq_url = self.generate_unique_link()
+
+    def generate_unique_link(self):
+        word = request.form['custom_word']
+        uniq_url = ''.join(word)
+
+        link2 = self.query.filter_by(uniq_url=uniq_url).first()
+
+        if link2:
+            return self.generate_unique_link()
+
+        return uniq_url
+
+
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template('index.html', )
 
@@ -47,37 +68,71 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+@app.route('/<short_url>')
+def redirect_to_url(short_url):
+    link = Link.query.filter_by(short_url=short_url).first_or_404()
 
+    link.visits = link.visits + 1
+    db.session.commit()
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('index'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return redirect(link.original_url)
 
 
 @app.route('/add_link', methods=['POST'])
-@login_required
 def add_link():
     original_url = request.form['original_url']
     link = Link(original_url=original_url)
     db.session.add(link)
     db.session.commit()
 
-    return render_template('link_added.html',
-                           new_link=link.short_url, original_url=link.original_url)
+    return render_template('link_added.html', new_link=link.short_url, original_url=link.original_url)
+
+
+@app.route("/stats")
+def stats():
+    links = Link.query.all()
+
+    return render_template("stats.html", links=links)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1>", 404
+
+
+@app.route('/premium')
+def prem_page():
+    return render_template("premium_page.html")
+
+
+@app.route('/<uniq_url>')
+def redirect_to_uniq_url(uniq_url):
+    link2 = Link2.query.filter_by(uniq_url=uniq_url).first_or_404()
+
+    link2.visits = link2.visits + 1
+
+    db.session.commit()
+
+    return redirect(link2.spec_url)
+
+
+@app.route("/add_spec_link", methods=['POST'])
+@requires_auth
+def add_special_link():
+    spec_url = request.form['spec_url']
+    link2 = Link2(spec_url=spec_url)
+
+    db.session.add(link2)
+    db.session.commit()
+
+    return render_template("premium_link_added.html", new_link=link2.uniq_url, spec_url=link2.spec_url)
+
+
+@app.route("/premium_stats")
+def premium():
+    links2 = Link2.query.all()
+
+    return render_template("premium_stats.html", links2=links2)
 
 
 if __name__ == '__main__':
